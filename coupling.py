c10accf39d23b3f94d95c0c9702ed9ed32e8562a
joheenc <joheen.c@gmail.com>
2021-06-24 11:11:45 -0400

    compute beam matrix

--- coupling.py
+++ coupling.py
@@ -2,17 +2,15 @@ import numpy as np
 import healpy as hp
 
 def compute_orbit(orbdec0=0, orbdec1=0, axrotPerOrb=20, ntimes=1000, norbits=20):
-	dslope = (orbdec1 - orbdec0) / ntimes
-	phis = np.linspace(0., norbits*360., ntimes)
-	return np.vstack([axrotPerOrb*phis, -phis, orbdec0+np.linspace(0., dslope, ntimes)]).T
+    dslope = (orbdec1 - orbdec0) / ntimes
+    phis = np.linspace(0., norbits*360., ntimes)
+    return np.vstack([axrotPerOrb*phis, -phis, orbdec0+np.linspace(0., dslope, ntimes)]).T
 
-def compute_B(beamfile, orbdec0=0, orbdec1=0, axrotPerOrb=20, ntimes=1000, norbits=20, lmax=15): #compute the beam matrix for a particular frequency
-	simbeam = hp.fitsfunc.read_map(beamfile)
-	alms = hp.map2alm(simbeam, lmax=lmax)
-	dslope = (orbdec1 - orbdec0) / ntimes
-	phis = np.linspace(0., norbits*360., ntimes)
-	B = np.zeros((ntimes, int((lmax+2)*(lmax+1)/2.0))) #B matrix stores alms (columns) at each t step (rows)
-	for rstep in range(ntimes):
-		rot = hp.Rotator(rot = [axrotPerOrb*phis[rstep], -phis[rstep], orbdec0+rstep*dslope], deg=True, eulertype='ZYX')
-		B[rstep] = rot.rotate_alm(alms, inplace=False))
-	return B
+def compute_B(beamfile, orbit, lmax=15):
+    simbeam = hp.fitsfunc.read_map(beamfile)
+    alms = hp.map2alm(simbeam, lmax=lmax)
+    B = np.full((len(orbit), int((lmax+2)*(lmax+1)/2.0)), 0.+0.j)
+    for rstep in range(len(orbit)):
+        rot = hp.Rotator(rot=orbit[rstep], deg=True, eulertype='ZYX')
+        B[rstep] = rot.rotate_alm(alms, inplace=False)
+    return B
